4cde6b64db08e9b274c7f57a75fb866aaf9396d7
Pradee <Prade@DESKTOP-H1VA3H5>
2016-11-10 12:48:39 -0600

    Initi version

---
+++ Test/src/main/java/com/harikrishna/Main.java
@@ -0,0 +1,20 @@
+package com.harikrishna;
+
+import java.util.Calendar;
+
+class Main<T> {
+	public static void main(String[] args) {
+/*		Integer i = new Integer();
+		Float f = new Float(23.43);
+		Boolean b = new Boolean("false");
+		Double d = new Double("17.6d");
+		Character d = new Character("d");
+		for(int i=5;i>1;i--){
+			System.out.print(i+"");
+		}*/
+	}
+	
+	public void meth(String[] args){
+		
+	}
+}
